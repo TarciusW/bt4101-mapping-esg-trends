2b3c625208af99f0b5e0e159211e2cda15c8cc65
TarciusW <e0325669@u.nus.edu>
2022-10-11 17:51:29 +0800

    Automated Scraping of data, from SNP 500 scraping tickers to mapping to CIK numbers and getting 10-K & 10-Q reports into 1 large df. Able to extend to the full SNP 500

--- data/main.py
+++ data/main.py
@@ -2,37 +2,93 @@ import pandas as pd
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 
-if __name__ == "__main__":
-    print("Launching webdriver...")
-    driver = webdriver.Chrome('C:/Users/Tarci/Documents/nus/Y4/FYP/chromedriver.exe')
-    driver.get('https://www.sec.gov/edgar/browse/?CIK=1411579')
+
+def get_snp_500_tickers() -> list:
+    # Get SNP 500 Tickers
+    driver.get('https://stockmarketmba.com/stocksinthesp500.php')
+    ticker_soup = BeautifulSoup(driver.page_source, 'html.parser')
+    tickers_row = ticker_soup.find_all("tr", {"role": "row"})
+    tickers = []
+    for row in tickers_row:
+        tickers.append(row.contents[0].get_text())
+    return tickers
+
+
+def get_CIK_from_tickers(tickers: list) -> pd.DataFrame:
+    # get_ticker_CIK_mapping_online
+    driver.get('https://www.sec.gov/include/ticker.txt')
+    cik_mapping_soup = BeautifulSoup(driver.page_source, 'html.parser')
+    cik_mapping_words = cik_mapping_soup.get_text()
+    cik_mapping_words = cik_mapping_words.split()
+    cik_dict = {}
+    for i in range(0, len(cik_mapping_words), 2):
+        if cik_mapping_words[i] == "brk-b":
+            cik_dict['BRK.B'] = cik_mapping_words[i + 1]
+        elif cik_mapping_words[i] == "bf-b":
+            cik_dict['BF.B'] = cik_mapping_words[i + 1]
+        else:
+            cik_dict[cik_mapping_words[i].upper()] = cik_mapping_words[i + 1]
+    ciks = []
+    # map current tickers
+    for i in tickers:
+        ciks.append(cik_dict[i])
+
+    tickers_df = pd.DataFrame({"Tickers": tickers, "CIK Number": ciks})
+    return tickers_df
+
+
+def get_10_reports_func(CIK: str) -> pd.DataFrame:
+    reports = []
+    dates = []
+    driver.get(f'https://www.sec.gov/edgar/browse/?CIK={CIK}')
+    ### Scrape 10K / 10Q forms
+    # driver.get('https://www.sec.gov/edgar/browse/?CIK=1411579')
     driver.find_element_by_xpath('//*[@id="filingsStart"]/div[2]/div[3]/h5').click()
     driver.implicitly_wait(5)
     driver.find_element_by_xpath('//*[@id="filingsStart"]/div[2]/div[3]/div/button[1]').click()
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    document_links = soup.find_all("a", {"class": "document-link"})
+    document_links = soup.find_all("tr", {"role": "row"})
     links = []
     for link in document_links:
-        if "10k" in link.attrs['aria-describedby'] or "10q" in link.attrs['aria-describedby']:
-            links.append(link)
-    text = []
-
+        if link.find("a", {"class": "document-link"}) == None:
+            continue
+        else:
+            links.append(link.find("a",{"class":"document-link"}))
+            dates.append(link.find("a",{"data-column":"Reporting Date"}).attrs['data-export'])
     for link in links:
         driver.get(f"https://www.sec.gov/{link.attrs['href']}")
         time.sleep(0.1)
-        soup = BeautifulSoup(driver.page_source,'html.parser')
-        text.append(soup.get_text())
-    """
-    driver.get(f"https://www.sec.gov/{links[0].attrs['href']}")
-    time.sleep(0.01)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    test = soup.get_text()
-    """
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        reports.append(soup.get_text())
 
+    test = pd.DataFrame({"Date":dates,"Report":reports})
+
+    return test
+
+
+def get_10_reports(df: pd.DataFrame) -> pd.DataFrame:
+    reports = pd.DataFrame()
+    for i, j in df.iterrows():
+        reports_df = get_10_reports_func(j['CIK Number'])
+        reports_df['Ticker'] = j['Tickers']
+        reports_df['CIK Number'] = j['CIK Number']
+        reports = pd.concat(reports,reports_df)
+    return reports
+
+
+if __name__ == "__main__":
+    print("Launching webdriver...")
+
+    chrome_options = Options()
+    # chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument("--disable-gpu")
+    #chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome('C:/Users/Tarci/Documents/nus/Y4/FYP/chromedriver.exe',options=chrome_options)
+    tickers = get_snp_500_tickers()
+    tickers = tickers[1:3]
+    stocks_df = get_CIK_from_tickers(tickers)
+    stocks_df = get_10_reports(stocks_df)
     print("Done...!")
