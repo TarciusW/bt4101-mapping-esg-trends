8ed089b70ce74ff002d2c4fffcedac0eb591b0d6
TarciusW <e0325669@u.nus.edu>
2022-10-13 16:52:37 +0800

    Refined looping for scraping

--- data/main.py
+++ data/main.py
@@ -12,15 +12,18 @@ chrome_options.add_argument("--disable-gpu")
 # chrome_options.add_argument("--headless")
 driver = webdriver.Chrome('C:/Users/Tarci/Documents/nus/Y4/FYP/chromedriver.exe', options=chrome_options)
 tickers_full = get_snp_500_tickers(driver)
-tickers = tickers_full[1:20]
+tickers = tickers_full[1:20] #Change this line to determine how many stocks to scrape out of the snp 500
 with open('snp_500_reports.json', 'r') as openfile:
     stocks_json = json.load(openfile)
 current_tickers = list(stocks_json.keys())
-tickers_left = list(filter(lambda x: x not in current_tickers, tickers))
-stocks_df = get_CIK_from_tickers(driver,tickers_left)
-stocks_df = get_10_reports(driver,stocks_df)
-stocks_dict = convert_df_to_dict(stocks_df, stocks_json)
-with open("snp_500_reports.json", "w") as outfile:
-    json.dump(stocks_dict, outfile)
-print(f"End of run...! Data is currently scraped for {len(list(stocks_dict.keys()))} stocks out of "
-      f"{len(tickers_full)}")
+while len(current_tickers) < len(tickers):
+    tickers_left = list(filter(lambda x: x not in current_tickers, tickers))
+    stocks_df = get_CIK_from_tickers(driver,tickers_left)
+    stocks_df = get_10_reports(driver,stocks_df)
+    stocks_dict = convert_df_to_dict(stocks_df, stocks_json)
+    stocks_json = stocks_dict
+    current_tickers = list(stocks_json.keys())
+    with open("snp_500_reports.json", "w") as outfile:
+        json.dump(stocks_dict, outfile)
+    print(f"End of run...! Data is currently scraped for {len(list(stocks_dict.keys()))} stocks out of "
+          f"{len(tickers)}")
