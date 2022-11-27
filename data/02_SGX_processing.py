64e54461c8c15504fd2b8d6a3569e5c0ac93b518
TarciusW <e0325669@u.nus.edu>
2022-11-27 22:22:27 +0800

    Refactor

--- data/02_SGX_processing.py
+++ data/02_SGX_processing.py
@@ -12,11 +12,10 @@ for filename in os.listdir(directory):
     if os.path.isfile(f):
         if 'DS_Store' in f or 'gitignore' in f:
             continue
-        file = open(f, "r",encoding="utf8").read()
-        df = pd.concat([df,pd.DataFrame({'File Name':[filename],'Text':[file]})])
+        file = open(f, "r", encoding="utf8").read()
+        df = pd.concat([df, pd.DataFrame({'File Name': [filename], 'Text': [file]})])
 df['Company Name'] = (df['File Name'].apply(lambda x: x.split(' (')))
 df['Company Name'] = df['Company Name'].apply(lambda x: x[0])
 df['Date'] = (df['File Name'].apply(lambda x: x.split(' ('))).apply(lambda x: x[1][0:7])
-
 
 print("")
