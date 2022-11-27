7493b8be0860918b55bd25a2f7c9f6ccc39379f8
TarciusW <e0325669@u.nus.edu>
2022-11-28 04:10:28 +0800

    rename to main as main script

---
+++ modelling/main.py
@@ -0,0 +1,11 @@
+from data_preprocessing_funcs import *
+
+# Importing & Preprocessing Data
+## Text Data
+# sgx = get_sgx_files()
+# snp = get_snp_files()
+
+## ESG Word List
+esg_dict = get_esg_wordlist()
+
+print("")
