5d9046563a299101e7a7b832169d9551007680a6
TarciusW <e0325669@u.nus.edu>
2023-01-07 07:23:33 +0800

    SGX pickle loaded and saved, can start to work on modelling

--- modelling/main.py
+++ modelling/main.py
@@ -4,11 +4,15 @@ import regex
 import string
 import pandas
 
+"""
 # Importing & Preprocessing Data and saving it to pickle files (Dataframe with tokens)
 sgx = preprocess_sgx_files()
 sgx.to_pickle('sgx.pkl')
 snp = preprocess_snp_files()
 snp.to_pickle(snp.pkl)
+"""
+
+sgx = pd.read_pickle('sgx.pkl')
 
 ## ESG Word List
 esg_dict = get_esg_wordlist()
