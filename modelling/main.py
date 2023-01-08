10f46545f2e0ee2649a6f9a768d177829bdc4318
TarciusW <e0325669@u.nus.edu>
2023-01-09 02:18:43 +0800

    Preliminary results for ESG % word count done, to refactor.

--- modelling/main.py
+++ modelling/main.py
@@ -1,21 +1,19 @@
 from data_preprocessing_funcs import *
-import nltk
-import regex
-import string
-import pandas
+from modelling_funcs import *
 
-"""
+
 # Importing & Preprocessing Data and saving it to pickle files (Dataframe with tokens)
-sgx = preprocess_sgx_files()
-sgx.to_pickle('sgx.pkl')
+#sgx = preprocess_sgx_files()
+#sgx.to_pickle('sgx.pkl')
+"""
 snp = preprocess_snp_files()
 snp.to_pickle(snp.pkl)
 """
 
 sgx = pd.read_pickle('sgx.pkl')
+model_tokens_BOW(sgx)
 
 ## ESG Word List
-esg_dict = get_esg_wordlist()
-
+#esg_dict = get_esg_wordlist()
 
 print("")
--- modelling/modelling_funcs.py
+++ modelling/modelling_funcs.py
@@ -1,5 +1,40 @@
-from bs4 import BeautifulSoup
-import re
-import nltk
-import seaborn as sns
-import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def model_tokens_BOW(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Takes in a df with the tokens and text files and runs the BOW model
+    """
+    df.reset_index(inplace=True)
+    df.drop(columns='index',inplace=True)
+    vectorizer = CountVectorizer()
+    results_df = vectorizer.fit_transform(df['Tokens'].apply(lambda x: ' '.join(x)))
+    test_df = pd.DataFrame(results_df.toarray(), columns=vectorizer.get_feature_names_out())
+
+    result = pd.DataFrame()
+    for index, row in test_df.iterrows():
+        row = row.reset_index()
+        row.rename(columns={'index': 'Word'}, inplace=True)
+        esg_dict = get_esg_wordlist()
+        esg_dict = esg_dict[['Word', 'Topic']].merge(row, how='left', on='Word').fillna(0)
+        # Topic Score
+        topic_score = esg_dict.groupby('Topic').sum(numeric_only = True)
+        result = pd.concat([result, topic_score.transpose()])
+        # Category Score (to-do)
+        # category_score = esg_dict[['Topic', 'Category',0]].groupby(['Topic', 'Category']).sum()
+        print("")
+    df = df.join(result)
+    df['Total'] = df['Tokens'].apply(lambda x: len(x))
+    df['E %'] = df['Environmental'] * 100 / df['Total']
+    df['S %'] = df['Social'] * 100 / df['Total']
+    df['G %'] = df['Governance'] * 100 / df['Total']
+    return df
+
+
+def get_esg_wordlist() -> pd.DataFrame:
+    """
+    loads the ESG word list to be used in this project.
+    """
+    return pd.read_excel('../data/ESG Word List/BaierBerningerKiesel_ESG-Wordlist_2020_July22.xlsx',
+                         sheet_name='ESG-Wordlist')
