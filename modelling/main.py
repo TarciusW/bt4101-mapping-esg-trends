21d230849b89b6906b9dd1fcad3ee24bc9cacb18
TarciusW <e0325669@u.nus.edu>
2023-01-09 20:48:04 +0800

    Exported results, now working to visualize preliminary results.

--- modelling/main.py
+++ modelling/main.py
@@ -1,19 +1,23 @@
 from data_preprocessing_funcs import *
 from modelling_funcs import *
-
+import matplotlib.pyplot as plt
 
 # Importing & Preprocessing Data and saving it to pickle files (Dataframe with tokens)
-#sgx = preprocess_sgx_files()
-#sgx.to_pickle('sgx.pkl')
+# sgx = preprocess_sgx_files()
+# sgx.to_pickle('sgx.pkl')
 """
 snp = preprocess_snp_files()
 snp.to_pickle(snp.pkl)
 """
 
+"""
+#Modelling quantitative Trends
 sgx = pd.read_pickle('sgx.pkl')
-model_tokens_BOW(sgx)
+sgx_quant = model_tokens_BOW(sgx)
+sgx_quant.to_pickle('sgx_quant.pkl')
+"""
 
-## ESG Word List
-#esg_dict = get_esg_wordlist()
-
+# Export Results of Trends
+sgx_quant = pd.read_pickle('sgx_quant.pkl')
+sgx_quant.drop(columns=['Tokens']).to_csv('sgx_quant_results.csv')
 print("")
