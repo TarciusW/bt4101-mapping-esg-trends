6ee0207d41b5dac0b5be995f4069a3b28aa4769f
TarciusW <e0325669@u.nus.edu>
2023-01-25 02:04:57 +0800

    Updated main

--- modelling/main.py
+++ modelling/main.py
@@ -1,6 +1,7 @@
+import pandas as pd
+
 from data_preprocessing_funcs import *
 from modelling_funcs import *
-import matplotlib.pyplot as plt
 
 # Importing & Preprocessing Data and saving it to pickle files (Dataframe with tokens)
 # sgx = preprocess_sgx_files()
@@ -19,5 +20,7 @@ sgx_quant.to_pickle('sgx_quant.pkl')
 
 # Export Results of Trends
 sgx_quant = pd.read_pickle('sgx_quant.pkl')
-sgx_quant.drop(columns=['Tokens']).to_csv('sgx_quant_results.csv')
+sgx_quant_processed = extract_substantial_companies(sgx_quant, 5)
+# ARA Trust Management has 5 files, 1 of it has (Cache) in the file name so doesnt count. manually remove.
+sgx_quant_processed = sgx_quant_processed[sgx_quant_processed['Company Name'] != 'ARA TRUST MANAGEMENT']
 print("")
