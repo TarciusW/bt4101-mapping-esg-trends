9a16e4e667af0c7f8c4cd17671030db101d3c706
TarciusW <e0325669@u.nus.edu>
2023-02-15 17:17:10 +0800

    updated main

--- modelling/main.py
+++ modelling/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from data_preprocessing_funcs import *
 from modelling_funcs import *
+from ESGBERT import *
 
 # Importing & Preprocessing Data and saving it to pickle files (Dataframe with tokens)
 # sgx = preprocess_sgx_files()
@@ -11,7 +12,7 @@ from modelling_funcs import *
 # snp.to_pickle('snp.pkl')
 
 """
-#Modelling quantitative Trends
+# Modelling quantitative Trends
 sgx = pd.read_pickle('sgx.pkl')
 sgx_quant = model_tokens_BOW(sgx)
 sgx_quant.to_pickle('sgx_quant.pkl')
@@ -30,19 +31,21 @@ sgx_quant_processed = extract_companies_with_trend(sgx_quant)
 """
 
 # Export Results of Trends
-"""
+
 sgx_quant = pd.read_pickle('sgx_quant.pkl')
 sgx_quant_processed = extract_substantial_companies(sgx_quant, 5)
-# ARA Trust Management has 5 files, 1 of it has (Cache) in the file name so doesnt count. manually remove.
+# ARA Trust Management has 5 files, 1 of it has (Cache) in the file name so doesn't count. manually remove.
 sgx_quant_processed = sgx_quant_processed[sgx_quant_processed['Company Name'] != 'ARA TRUST MANAGEMENT']
 sgx_quant_processed.to_excel('sgx_quant_results.xlsx')
-"""
+
+#sgx_quant_processed['String'] = sgx_quant_processed['Tokens'].apply(lambda x: ' '.join(x))
+#sgx_quant_processed['BERT_Tokens'] = sgx_quant_processed['String'].apply(lambda x: tokenizer(x))
 
 
 # Export Results of Trends
-snp_quant = pd.read_pickle('snp_quant.pkl')
-snp_quant_results = extract_substantial_companies(snp_quant, 5)
-snp_quant_results.to_excel('snp_quant_results.xlsx')
-snp_quant_processed = extract_companies_with_trend(snp_quant)
+# snp_quant = pd.read_pickle('snp_quant.pkl')
+# snp_quant_results = extract_substantial_companies(snp_quant, 5)
+# snp_quant_results.to_excel('snp_quant_results.xlsx')
+# snp_quant_processed = extract_companies_with_trend(snp_quant)
 
 print("")
