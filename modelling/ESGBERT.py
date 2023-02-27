b2a2368657b8c00a33d21d678834bf76db68ae75
TarciusW <e0325669@u.nus.edu>
2023-02-28 03:30:21 +0800

    Wrote code for token reducing and esgbert for snp tokens, need to fix token reducing

--- modelling/ESGBERT.py
+++ modelling/ESGBERT.py
@@ -2,12 +2,41 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from modelling_funcs import *
 import torch
-from tqdm import tqdm
 
-sgx_quant = pd.read_pickle('sgx_quant.pkl')
-sgx_quant['Sentences'] = sgx_quant['Tokens'].apply(
-    lambda y: [' '.join(x) for x in
-               zip(y[0::9], y[1::9], y[2::9], y[3::9], y[4::9], y[5::9], y[6::9], y[7::9], y[8::9])])
+
+def join_sentences(list):
+    combined_strings = []
+    for i in range(10, len(list) - 10, 10):
+        combined_strings.append(" ".join(list[i - 10:i + 10]))
+    return combined_strings
+
+
+def reduce_snp_tokens(df):
+    df['Report Type'] = df['Quarter'].apply(lambda x: 'Annual' if 'Q4' in str(x) else 'Quarterly')
+    df.loc[:, 'Tokens_ESGB'] = 'x'
+
+    for index, row in df.iterrows():
+        text = df.loc[index, 'Tokens']
+        if 'Q4' in str(row['Quarter']):
+            index_req = []
+            # Annual Reports, need to take item 1 to 7
+            # first, find all "business" forstart index
+            item_1_business_indexes = [i for i, j in enumerate(text) if 'business' in j]
+            # next, check if token before is '1' and token after is 'overview'
+            start_index = [i for i in item_1_business_indexes if 'overview' in text[i + 1]]
+            item_7A_qualitative_indexes = [i for i, j in enumerate(text) if j == 'qualitative']
+            end_index = [i for i in item_7A_qualitative_indexes if 'disclosure' in text[i + 1]]
+            text = text[start_index[0]:end_index[-1]]
+            df.loc[:, 'Tokens_ESGB'].loc[index] = text
+        else:
+            # quarterly reports, need to take item 2 until item 6
+            item_2_mdna = [i for i, j in enumerate(text) if 'discussion' in j]
+            start_index = [i for i in item_2_mdna if 'analysis' in text[i + 1]]
+            item_2_unregistered = [i for i, j in enumerate(text) if 'unregistered' in j]
+            end_index = [i for i in item_2_unregistered if 'sale' in text[i + 1]]
+            text = text[start_index[1]:end_index[-1]]
+            df.loc[:, 'Tokens_ESGB'].loc[index] = text
+    return df
 
 
 def ESGBERT_clf(text_list: list) -> pd.DataFrame:
@@ -49,34 +78,28 @@ def ESGBERT_clf(text_list: list) -> pd.DataFrame:
         result = model.config.id2label[logits.argmax().item()]
         result_loss = logits.max().item()
         result_mapped = esg_labels_map[result]
-        if float(result_loss) < 6:
+        if (result_mapped == 'G' and float(result_loss) < 3) or (result_mapped == 'S' and float(result_loss) < 1):
             result_mapped = "NIL"
         return [result, result_loss, result_mapped]
 
-    result_df = pd.DataFrame()
-    for i in tqdm(text_list, total=len(text_list)):
-        result_df = pd.concat([result_df, pd.DataFrame(pd.Series(ESGBERT(i, model, tokenizer))).transpose()])
-    result_df.rename(columns={0: 'Label', 1: "Score", 2: 'ESG'}, inplace=True)
-    percentage_df = pd.DataFrame(result_df['ESG'].value_counts().astype(int)).transpose()
-    percentage_df['Total'] = percentage_df.sum(axis=1)
     final_df = pd.DataFrame()
+    result_df = pd.DataFrame()
+    for item in text_list:
+        try:
+            interim_df = pd.DataFrame(list(map(ESGBERT, item, [model], [tokenizer])))
+            interim_df.rename(columns={0: 'Label', 1: "Score", 2: 'ESG'}, inplace=True)
+            result_df = pd.concat([result_df, interim_df])
+        except:
+            continue
+    percentage_df = pd.DataFrame(columns=["E", "S", "G"])
+    percentage_df = pd.concat(
+        [percentage_df, pd.DataFrame(result_df['ESG'].value_counts().astype(int)).transpose()])
+    percentage_df['Total'] = percentage_df.sum(axis=1)
     final_df['E % BERT'] = percentage_df['E'] / percentage_df['Total'] * 100
     final_df['S % BERT'] = percentage_df['S'] / percentage_df['Total'] * 100
     final_df['G % BERT'] = percentage_df['G'] / percentage_df['Total'] * 100
-    return result_df
-
-
-ESGBERT_clf(sgx_quant['Sentences'][0])
-
-"""
-num_labels = len(model.config.id2label)
-labels = torch.tensor([1])
-loss = model(**inputs, labels=labels).loss
-"""
-
-"""
-sgx_quant_processed['String'] = sgx_quant_processed['Tokens'].apply(lambda x: ' '.join(x))
-sgx_quant_processed['BERT_Tokens'] = sgx_quant_processed['String'].apply(lambda x: tokenizer(x))
-"""
-
-print("")
+    final_df['E BERT'] = percentage_df['E']
+    final_df['S BERT'] = percentage_df['S']
+    final_df['G BERT'] = percentage_df['G']
+    final_df['Total BERT'] = percentage_df['Total']
+    return final_df.reset_index().drop(columns=['index'])
