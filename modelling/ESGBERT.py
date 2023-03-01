3bc28f03b9db974e571cec77c0e1873aff0ad95d
TarciusW <e0325669@u.nus.edu>
2023-03-01 10:17:39 +0800

    Updated tokens reducing, snp ready for processing ESGB

--- modelling/ESGBERT.py
+++ modelling/ESGBERT.py
@@ -2,6 +2,7 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from modelling_funcs import *
 import torch
+from datetime import datetime
 
 
 def join_sentences(list):
@@ -11,31 +12,76 @@ def join_sentences(list):
     return combined_strings
 
 
-def reduce_snp_tokens(df):
-    df['Report Type'] = df['Quarter'].apply(lambda x: 'Annual' if 'Q4' in str(x) else 'Quarterly')
-    df.loc[:, 'Tokens_ESGB'] = 'x'
+def tag_report_type(df):
+    tickers = df['Ticker'].unique()
+    final_df = pd.DataFrame()
+    for i in tickers:
+        ticker_df = df[df['Ticker'] == i].copy()
+        # ticker_df['Date'] = ticker_df['Date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d').date())
+        # report_stats = ticker_df['Total'].describe()
+        ticker_df['Report Type'] = ticker_df['Total'].apply(
+            lambda x: 'Annual' if x > ticker_df['Total'].describe()['75%'] else 'Quarterly')
+        """
+        if 1 in ticker_df['total'].apply(lambda x: x.month).tolist():
+            ticker_df['Report Type'] = ticker_df['Quarter'].apply(lambda x: 'Annual' if 'Q1' in str(x) else 'Quarterly')
+        else:
+            ticker_df['Report Type'] = ticker_df['Quarter'].apply(lambda x: 'Annual' if 'Q4' in str(x) else 'Quarterly')
+        """
+        final_df = pd.concat([final_df, ticker_df])
+    return final_df
 
+
+def reduce_snp_tokens(df):
+    # need to find out which companies report before year end which companies report annual after year end
+    df.loc[:, 'Tokens_ESGB'] = 'x'
     for index, row in df.iterrows():
         text = df.loc[index, 'Tokens']
-        if 'Q4' in str(row['Quarter']):
-            index_req = []
+        try:
             # Annual Reports, need to take item 1 to 7
-            # first, find all "business" forstart index
+            # first, find all "business" for start index
             item_1_business_indexes = [i for i, j in enumerate(text) if 'business' in j]
             # next, check if token before is '1' and token after is 'overview'
-            start_index = [i for i in item_1_business_indexes if 'overview' in text[i + 1]]
-            item_7A_qualitative_indexes = [i for i, j in enumerate(text) if j == 'qualitative']
-            end_index = [i for i in item_7A_qualitative_indexes if 'disclosure' in text[i + 1]]
+            start_index = [i for i in item_1_business_indexes if 'item' in text[i - 1] or 'item' in text[i]]
+            item_7A_qualitative_indexes = [i for i, j in enumerate(text) if 'qualitative' in j]
+            end_index = [i for i in item_7A_qualitative_indexes if
+                         'disclosure' in text[i + 1] or 'disclosure' in text[i]]
+            if start_index[0] > 5000 or end_index[-1] < 5000:
+                raise Exception
             text = text[start_index[0]:end_index[-1]]
             df.loc[:, 'Tokens_ESGB'].loc[index] = text
-        else:
-            # quarterly reports, need to take item 2 until item 6
-            item_2_mdna = [i for i, j in enumerate(text) if 'discussion' in j]
-            start_index = [i for i in item_2_mdna if 'analysis' in text[i + 1]]
-            item_2_unregistered = [i for i, j in enumerate(text) if 'unregistered' in j]
-            end_index = [i for i in item_2_unregistered if 'sale' in text[i + 1]]
-            text = text[start_index[1]:end_index[-1]]
-            df.loc[:, 'Tokens_ESGB'].loc[index] = text
+        except:
+            try:
+                # quarterly reports, need to take item 2 until item 6
+                item_2_mdna = [i for i, j in enumerate(text) if 'discussion' in j]
+                start_index = [i for i in item_2_mdna if 'analysis' in text[i + 1]]
+                # item_6_exhibit = [i for i, j in enumerate(text) if 'exhibit' in j]
+                # end_index = [i for i in item_6_exhibit if 'item' in text[i - 1]]
+                text = text[start_index[1]:]
+                df.loc[:, 'Tokens_ESGB'].loc[index] = text
+            except:
+                print(index)
+            """
+            text = df.loc[index, 'Tokens']
+            if row['Report Type'] == 'Annual':
+                # Annual Reports, need to take item 1 to 7
+                # first, find all "business" for start index
+                item_1_business_indexes = [i for i, j in enumerate(text) if 'business' in j]
+                # next, check if token before is '1' and token after is 'overview'
+                start_index = [i for i in item_1_business_indexes if 'item' in text[i - 1] or 'item' in text[i]]
+                item_7A_qualitative_indexes = [i for i, j in enumerate(text) if 'qualitative' in j]
+                end_index = [i for i in item_7A_qualitative_indexes if
+                             'disclosure' in text[i + 1] or 'disclosure' in text[i]]
+                text = text[start_index[0]:end_index[-1]]
+                df.loc[:, 'Tokens_ESGB'].loc[index] = text
+            else:
+                # quarterly reports, need to take item 2 until item 6
+                item_2_mdna = [i for i, j in enumerate(text) if 'discussion' in j]
+                start_index = [i for i in item_2_mdna if 'analysis' in text[i + 1]]
+                # item_6_exhibit = [i for i, j in enumerate(text) if 'exhibit' in j]
+                # end_index = [i for i in item_6_exhibit if 'item' in text[i - 1]]
+                text = text[start_index[1]:]
+                df.loc[:, 'Tokens_ESGB'].loc[index] = text
+            """
     return df
 
 
@@ -78,8 +124,9 @@ def ESGBERT_clf(text_list: list) -> pd.DataFrame:
         result = model.config.id2label[logits.argmax().item()]
         result_loss = logits.max().item()
         result_mapped = esg_labels_map[result]
-        if (result_mapped == 'G' and float(result_loss) < 3) or (result_mapped == 'S' and float(result_loss) < 1):
-            result_mapped = "NIL"
+        #if (result_mapped == 'G' and float(result_loss) < 3) or (result_mapped == 'S' and float(result_loss) < 1):
+        result_mapped = "NIL"
+
         return [result, result_loss, result_mapped]
 
     final_df = pd.DataFrame()
@@ -93,7 +140,7 @@ def ESGBERT_clf(text_list: list) -> pd.DataFrame:
             continue
     percentage_df = pd.DataFrame(columns=["E", "S", "G"])
     percentage_df = pd.concat(
-        [percentage_df, pd.DataFrame(result_df['ESG'].value_counts().astype(int)).transpose()])
+        [percentage_df, pd.DataFrame(result_df['ESG'].value_counts().astype(int)).transpose()]).fillna(0)
     percentage_df['Total'] = percentage_df.sum(axis=1)
     final_df['E % BERT'] = percentage_df['E'] / percentage_df['Total'] * 100
     final_df['S % BERT'] = percentage_df['S'] / percentage_df['Total'] * 100
