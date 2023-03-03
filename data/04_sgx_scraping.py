296a25d5259e182dcdbf53f119402cce6100eaf9
TarciusW <e0325669@u.nus.edu>
2023-03-04 00:19:20 +0800

    Code to scrape annual reports for STI's companies

--- data/04_sgx_scraping.py
+++ data/04_sgx_scraping.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.select import Select
 import pandas as pd
 from bs4 import BeautifulSoup
-from tqdm import tqdm
 import time
 import os
 
@@ -81,47 +79,53 @@ def get_10_reports_func(driver, CIK: str, Ticker: str) -> pd.DataFrame:
 def get_STI_reports(driver, companies) -> pd.DataFrame:
     print("Scraping STI reports...")
     for company in companies:
-        driver.get(f'https://www.sgx.com/securities/annual-reports-related-documents')
-        driver.implicitly_wait(5)
-        search_box = driver.find_element_by_xpath(
-            '//*[@id="page-container"]/template-base/div/div/sgx-widgets-wrapper/widget-filter-listing/widget-filter-listing-financial-reports/section/div[1]/sgx-filter-bar/sgx-input-select/label/span[2]/input')
-        search_box.send_keys(f"{company}")
-        driver.implicitly_wait(5)
-        options = driver.find_elements_by_class_name("sgx-select-picker-label")
-        driver.implicitly_wait(5)
-        # click the correct option
-        options[list(map(lambda x: x.text, options)).index(company)].click()
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        rows = soup.find_all("sgx-table-row")
-        # extract dates and check if dates are not none, and check that the rows are Annual Report Rows
-        annual_report_rows = list(filter(lambda item: item != None, list(
-            map(lambda x: x if x.contents[3].text == 'Annual Report' else None, rows))))
-        company_name = annual_report_rows[0].contents[0].text
-        security_name = annual_report_rows[0].contents[1].text
-        dates = list(map(lambda x: x.contents[2].text, annual_report_rows))
-        links = list(map(lambda x: x.contents[3].contents[0]['href'], annual_report_rows))
-        for i in links:
-            driver.get(f'https://www.sgx.com/securities/annual-reports-related-documents')
-
-        """
-
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        
-        table = soup.find_all("sgx-table-list")
-        """
-
-        print("")
-
-    """
-    for i, j in tqdm(df.iterrows()):
-        print(f"\nScraping STI reports for {j['Tickers']}...")
-        reports_df = get_10_reports_func(driver, j['CIK Number'], j['Tickers'])
-        reports_df['Ticker'] = j['Tickers']
-        reports_df['CIK Number'] = j['CIK Number']
-        reports = pd.concat([reports, reports_df])
-    print("... done!")
-    """
-    return reports
+        for attempt in range(10):
+            try:
+                driver.get(f'https://www.sgx.com/securities/annual-reports-related-documents')
+                time.sleep(1)
+                search_box = driver.find_element_by_xpath(
+                    '//*[@id="page-container"]/template-base/div/div/sgx-widgets-wrapper/widget-filter-listing/widget-filter-listing-financial-reports/section/div[1]/sgx-filter-bar/sgx-input-select/label/span[2]/input')
+                search_box.send_keys(f"{company}")
+                time.sleep(1)
+                options = driver.find_elements_by_class_name("sgx-select-picker-label")
+                time.sleep(1)
+                # click the correct option
+                try:
+                    options[list(map(lambda x: x.text, options)).index(company)].click()
+                except:
+                    print(f"Could not find {company}... Continuing")
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                rows = soup.find_all("sgx-table-row")
+                # extract dates and check if dates are not none, and check that the rows are Annual Report Rows
+                annual_report_rows = list(filter(lambda item: item != None, list(
+                    map(lambda x: x if x.contents[3].text == 'Annual Report' and x.contents[
+                        1].text == company else None,
+                        rows))))
+                company_name = annual_report_rows[0].contents[0].text
+                security_name = annual_report_rows[0].contents[1].text
+                dates = list(map(lambda x: x.contents[2].text, annual_report_rows))
+                links = list(map(lambda x: x.contents[3].contents[0]['href'], annual_report_rows))
+                for i in range(len(links)):
+                    driver.get(links[i])
+                    option = driver.find_elements_by_class_name("announcement-attachment")
+                    # get document link
+                    report_link_list = [k for k in option if
+                                        "Annual Report" in k.text or "AR" in k.text or "AnnualReport" in k.text]
+                    if len(report_link_list) == 0:
+                        continue
+                    report_link = report_link_list[0].get_attribute('href')
+                    # access report
+                    driver.get(report_link)
+                    time.sleep(0.5)
+                    """
+                    soup = BeautifulSoup(driver.page_source, 'html.parser')
+                    with open(f"{directory}/{company_name} {security_name} {dates[i]}.txt", "w", encoding="utf-8") as text_file:
+                        text_file.write(soup.get_text())
+                    """
+            except:
+                print(f"Failed! try number {attempt} out of 10 for {company}")
+            else:
+                break
 
 
 current_tickers = get_current_tickers()
@@ -131,7 +135,13 @@ print("Launching webdriver....")
 chrome_options = Options()
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
-driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_options.add_experimental_option('prefs', {
+    "download.default_directory": directory,  # Change default directory for downloads
+    "download.prompt_for_download": False,  # To auto download the file
+    "download.directory_upgrade": True,
+    "plugins.always_open_pdf_externally": True  # It will not show PDF directly in chrome
+})
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 companies_full = get_STI_company_names(driver)
 tickers_left = list(filter(lambda x: x not in current_tickers, companies_full))
 get_STI_reports(driver, tickers_left)
