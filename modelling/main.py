f4dff32f0d9e3cd3376f92a70238446486a25c16
TarciusW <e0325669@u.nus.edu>
2023-03-15 03:24:00 +0800

    updated main

--- modelling/main.py
+++ modelling/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 from data_preprocessing_funcs import *
 from modelling_funcs import *
 from ESGBERT import *
@@ -11,11 +10,11 @@ import numpy as np
 # sgx = preprocess_sgx_files()
 # sgx.to_pickle('sgx.pkl')
 
-# snp = preprocess_snp_files()
-# snp.to_pickle('snp.pkl')
+#snp = preprocess_snp_files()
+#snp.to_pickle('snp.pkl')
 
-#sgx_ann = preprocess_sgx_annual_files()
-#sgx_ann.to_pickle('sgx_ann.pkl')
+sgx_ann = preprocess_sgx_annual_files()
+sgx_ann.to_pickle('sgx_ann.pkl')
 
 """
 # Modelling quantitative Trends
@@ -24,10 +23,46 @@ sgx_quant = model_tokens_BOW(sgx)
 sgx_quant.to_pickle('sgx_quant.pkl')
 """
 
-# snp = pd.read_pickle('snp.pkl')
-# snp_quant = model_tokens_BOW(snp)
-# snp_quant.to_pickle('snp_quant.pkl')
+#snp = pd.read_pickle('snp.pkl')
+#snp_quant = model_tokens_BOW(snp)
+#snp_quant.to_pickle('snp_quant.pkl')
 
+""" SNP TOKEN REDUCING """
+"""
+snp_quant = pd.read_pickle('snp_quant.pkl')
+snp_quant = snp_quant[6000:6800]
+snp_quant = tag_report_type(snp_quant)
+snp_quant = reduce_snp_tokens(snp_quant)
+snp_quant['Sentences'] = snp_quant['Tokens_ESGB'].apply(lambda x: join_sentences(x))
+tqdm.pandas(total=len(snp_quant))
+snp_bert = snp_quant['Sentences'].progress_apply(lambda x: ESGBERT_clf(x))
+snp_bert = pd.concat(snp_bert.tolist()).reset_index().drop(columns=['index'])
+snp_quant_bert = snp_quant.join(snp_bert)
+snp_quant_bert.to_pickle('snp_quant_bert_6000_6800.pkl')
+"""
+
+# Export Results of Trends
+
+# sgx_quant_1 = pd.read_pickle('sgx_quant_bert_0_700.pkl')
+# sgx_quant_2 = pd.read_pickle('sgx_quant_bert_700.pkl')
+# sgx_quant = pd.concat([sgx_quant_1, sgx_quant_2])
+
+"""
+sgx_quant = pd.read_pickle('sgx_quant.pkl')
+sgx_quant_processed = extract_companies_with_trend(sgx_quant)
+sgx_quant_processed.to_excel('sgx_quant_results.xlsx')
+# ARA Trust Management has 5 files, 1 of it has (Cache) in the file name so doesn't count. manually remove.
+# sgx_quant_processed = sgx_quant_processed[sgx_quant_processed['Company Name'] != 'ARA TRUST MANAGEMENT']
+# sgx_quant_processed.to_excel('sgx_quant_results.xlsx')
+"""
+
+# Export Results of Trends
+#
+# snp_quant_results = extract_substantial_companies(snp_quant, 5)
+# snp_quant_results.to_excel('snp_quant_results.xlsx')
+# snp_quant_processed = extract_companies_with_trend(snp_quant)
+
+print("")
 
 """ ESGBERT """
 """
@@ -43,35 +78,3 @@ sgx_quant_bert = sgx_quant.join(sgx_bert)
 sgx_quant_bert.to_pickle('sgx_quant_bert_700.pkl')
 """
 """ END ESGBERT """
-
-""" SNP TOKEN REDUCING """
-"""
-snp_quant = pd.read_pickle('snp_quant.pkl')
-snp_quant = snp_quant[1900:2500]
-snp_quant = tag_report_type(snp_quant)
-snp_quant = reduce_snp_tokens(snp_quant)
-snp_quant['Sentences'] = snp_quant['Tokens_ESGB'].apply(lambda x: join_sentences(x))
-tqdm.pandas(total=len(snp_quant))
-snp_bert = snp_quant['Sentences'].progress_apply(lambda x: ESGBERT_clf(x))
-snp_bert = pd.concat(snp_bert.tolist()).reset_index().drop(columns=['index'])
-snp_quant_bert = snp_quant.join(snp_bert)
-snp_quant_bert.to_pickle('snp_quant_bert_1900_2500.pkl')
-"""
-
-"""
-# Export Results of Trends
-
-sgx_quant = pd.read_pickle('sgx_quant.pkl')
-sgx_quant_processed = extract_substantial_companies(sgx_quant, 5)
-# ARA Trust Management has 5 files, 1 of it has (Cache) in the file name so doesn't count. manually remove.
-sgx_quant_processed = sgx_quant_processed[sgx_quant_processed['Company Name'] != 'ARA TRUST MANAGEMENT']
-sgx_quant_processed.to_excel('sgx_quant_results.xlsx')
-"""
-
-# Export Results of Trends
-#
-# snp_quant_results = extract_substantial_companies(snp_quant, 5)
-# snp_quant_results.to_excel('snp_quant_results.xlsx')
-# snp_quant_processed = extract_companies_with_trend(snp_quant)
-
-print("")
